6e9b0311fdb48b70b861bc78eb70f34fc11b73f5
Oleksandr Andriichenko <panteras88smm@gmail.com>
2025-11-10 23:16:47 +0200

    add pagination

---
+++ src/routes/notesRoutes.js
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+import {
+	getAllNotes,
+  getNoteById,
+  createNote,
+  deleteNote,
+  updateNote
+} from '../controllers/notesController.js';
+import { celebrate } from 'celebrate';
+import {
+  createNoteSchema,
+  getAllNotesSchema,
+  noteIdSchema,
+  updateStudentSchema
+} from '../validations/notesValidation.js';
+
+const router = Router();
+
+router.get('/notes', celebrate(getAllNotesSchema), getAllNotes);
+router.get('/notes/:noteId', celebrate(noteIdSchema), getNoteById);
+router.post('/notes', celebrate(createNoteSchema), createNote);
+router.delete("/notes/:noteId", celebrate(noteIdSchema), deleteNote);
+router.patch('/notes/:noteId', celebrate(updateStudentSchema), updateNote);
+
+export default router;
